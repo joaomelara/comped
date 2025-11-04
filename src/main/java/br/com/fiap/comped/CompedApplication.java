2bdca00bd3b685946ecc4e780700f4bdc4a5870b
joaomelara <joaopedromelara@gmail.com>
2025-11-03 22:27:15 -0300

    firt commit

---
+++ src/main/java/br/com/fiap/comped/CompedApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.comped;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompedApplication.class, args);
+	}
+
+}
