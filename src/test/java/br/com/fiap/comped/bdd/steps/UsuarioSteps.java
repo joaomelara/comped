1186833987475a9ae8e9c8e5a0eedc511b773fd7
jmelara <joaopedromelara@gmail.com>
2026-05-10 05:09:14 -0300

    tests started, initial usuario bdd

---
+++ src/test/java/br/com/fiap/comped/bdd/steps/UsuarioSteps.java
@@ -0,0 +1,75 @@
+package br.com.fiap.comped.bdd.steps;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.E;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+
+public class UsuarioSteps {
+    @Dado("que eu tenha os seguintes dados do usuário:")
+    public void queEuTenhaOsSeguintesDadosDoUsuário() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Quando("eu enviar a requisição para o endpoint {string} de cadastro de usuários")
+    public void euEnviarARequisiçãoParaOEndpointDeCadastroDeUsuários(String arg0) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Então("o status code da resposta deve ser {int}")
+    public void oStatusCodeDaRespostaDeveSer(int arg0) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Dado("que não existe usuário com ID {int}")
+    public void queNãoExisteUsuárioComID(int arg0) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Quando("eu enviar a requisição de busca para o endpoint {string}")
+    public void euEnviarARequisiçãoDeBuscaParaOEndpoint(String arg0) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Dado("que existe pelo menos um usuário cadastrado no sistema")
+    public void queExistePeloMenosUmUsuárioCadastradoNoSistema() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Quando("eu enviar a requisição para o endpoint {string} de listagem de usuários")
+    public void euEnviarARequisiçãoParaOEndpointDeListagemDeUsuários(String arg0) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Dado("que existe um usuário cadastrado no sistema")
+    public void queExisteUmUsuárioCadastradoNoSistema() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @E("que eu tenha os seguintes dados atualizados:")
+    public void queEuTenhaOsSeguintesDadosAtualizados() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Quando("eu enviar a requisição de atualização para o endpoint {string}")
+    public void euEnviarARequisiçãoDeAtualizaçãoParaOEndpoint(String arg0) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Quando("eu enviar a requisição de exclusão para o endpoint {string}")
+    public void euEnviarARequisiçãoDeExclusãoParaOEndpoint(String arg0) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
